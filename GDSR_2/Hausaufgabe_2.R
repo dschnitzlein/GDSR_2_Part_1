d88ebaea4be51c3e99b5fa6708d760074657f7d2
dschnitzlein <127105983+dschnitzlein@users.noreply.github.com>
2024-12-06 12:55:41 +0100

    adds Lösung Hausaufgabe

---
+++ GDSR_2/Hausaufgabe_2.R
@@ -0,0 +1,87 @@
+rm(list=ls())
+
+library(tidyverse)
+library(ggthemes)
+
+cars_df <- read_csv("Documents/THD/Lehre/InnStat/GrundlagenR/Skript/GDSR_1_Part_1/Files for Download/ToyotaCorolla.csv")
+
+View(cars_df)
+
+test <- na.omit(cars_df)  # hat gleiche Dimension wie cars_df, also kein Handlungsbedarf.
+rm(test)
+
+dim(cars_df) # 1436 Beobachtungen und 39 Variablen
+
+## Analyse
+
+Price_mw <- mean(cars_df$Price)
+KM_mw <- mean(cars_df$KM)
+
+ggplot(data=cars_df, aes(x=KM, y=Price))
+
+ggplot(data=cars_df, aes(x=KM, y=Price)) +
+  geom_point()
+
+ggplot(data=cars_df, aes(x=KM, y=Price, color=Fuel_Type)) +
+  geom_point()
+
+ggplot(data=cars_df, aes(x=KM, y=Price)) +
+  geom_point(aes(color=Fuel_Type)) +
+  geom_smooth(method = 'lm')
+
+ggplot(data=cars_df, aes(x=KM, y=Price)) +
+  geom_point(aes(color=Fuel_Type, shape=Fuel_Type)) +
+  geom_smooth(method = 'lm')
+
+ggplot(data=cars_df, aes(x=KM, y=Price)) +
+  geom_point(aes(color=Fuel_Type, shape=Fuel_Type)) +
+  geom_smooth(method = 'lm') +
+  labs(
+    title="Der Zusammenhang zwischen Preis und Kilometerstand",
+    subtitle="Daten aus dem Toyota Corolla Datensatz",
+    x= "Gefahrene KM",
+    y= "Verkaufspreis",
+    color= "Kraftstoff",
+    shape= "Kraftstoff")
+
+ggplot(data=cars_df, aes(x=KM, y=Price)) +
+  geom_point(aes(color=Fuel_Type, shape=Fuel_Type)) +
+  geom_smooth(method = 'lm') +
+  geom_hline(yintercept=Price_mw, color='blue', linetype=3) + 
+  geom_vline(xintercept=KM_mw, color='blue', linetype=3) + 
+    labs(
+    title="Der Zusammenhang zwischen Preis und Kilometerstand",
+    subtitle="Daten aus dem Toyota Corolla Datensatz",
+    x= "Gefahrene KM",
+    y= "Verkaufspreis",
+    color= "Kraftstoff",
+    shape= "Kraftstoff")
+
+ggplot(data=cars_df, aes(x=KM, y=Price)) +
+  geom_point(aes(color=Fuel_Type, shape=Fuel_Type)) +
+  geom_smooth(method = 'lm') +
+  geom_hline(yintercept=Price_mw, color='blue', linetype=3) + 
+  geom_vline(xintercept=KM_mw, color='blue', linetype=3) + 
+  labs(
+    title="Der Zusammenhang zwischen Preis und Kilometerstand",
+    subtitle="Daten aus dem Toyota Corolla Datensatz",
+    x= "Gefahrene KM",
+    y= "Verkaufspreis",
+    color= "Kraftstoff",
+    shape= "Kraftstoff") +
+  scale_color_colorblind() +
+  theme_bw()
+
+ggplot(data=cars_df, aes(x=KM, y=Price)) +
+  geom_point(aes(color=Fuel_Type, shape=Fuel_Type)) +
+  geom_smooth(method = 'lm') +
+  facet_wrap(~Fuel_Type) +
+  labs(
+    title="Der Zusammenhang zwischen Preis und Kilometerstand",
+    subtitle="Daten aus dem Toyota Corolla Datensatz",
+    x= "Gefahrene KM",
+    y= "Verkaufspreis",
+    color= "Kraftstoff",
+    shape= "Kraftstoff") +
+  scale_color_colorblind() +
+  theme_bw()
